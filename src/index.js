dcde4eb1c8b461c2824422b19774449b5905cd51
tyrannosaurus-lee <miaow106@gmail.com>
2024-03-10 20:27:37 +0900

    멘토 바꾸기 - 연습(중첩 객체 상태 관리)

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import AppXY from './AppXY';
+// import AppXY from './AppXY';
+import AppMentor from './AppMentor';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppXY />
+    <AppMentor />
   </React.StrictMode>
 );
 
